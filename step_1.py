710c65fb3c53669f12013dc52c594d00f1435353
Eli <ilyashegurov@gmail.com>
2021-10-24 20:44:18 +0300

    my test github

---
+++ step_1.py
@@ -0,0 +1,11 @@
+# my_var_1 = 5
+# my_var_2 = '5'
+# print(my_var_1 == my_var_2)
+# print(my_var_1 === my_var_2)
+# str = 'hello'
+
+# age = input()
+age = input('введите ваш возраст: ')
+print('ваш возраст:', age)
+
+
